3449075b18aa5ee1a30c8513ef3a1ccf0c762d9f
Ryan Zander <exide@hotmail.com>
2016-06-15 13:59:31 -0700

    Added a way to scan starting at a specific date.

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ Simple tool for scanning a [Hub](http://github.com/flightstats/hub) channel for 
     $ node scanBackwardsFromLatest.js http://hub/channel/foo bar
 
 This will start at the latest item in channel 'foo' and walk backwards, scanning the contents of each item looking for 'bar'. Scanning will only stop when reaching the beginning. I recommend killing the script (CTRL-C) when you're done scanning.
+
+## scanBackwardsFromDate
+
+    $ node scanBackwardsFromDate.js http://hub/channel/foo bar 1999-12-31
+
+This operates the same as ```scanBackwardsFromLatest``` but instead of starting from ```latest``` it starts at the last entry on the date provided.
--- scanBackwardsFromDate.js
+++ scanBackwardsFromDate.js
@@ -4,6 +4,7 @@ var HubClient = require('./lib/hubClient');
 var URI = require('URIjs');
 var channelURI = process.argv[2];
 var keyword = process.argv[3];
+var timestamp = process.argv[4];
 
 var hubURI = URI(channelURI).authority();
 var hubClient = new HubClient(hubURI);
@@ -11,15 +12,24 @@ var uriPath = URI(channelURI).path();
 var channelName = uriPath.substr(uriPath.lastIndexOf('/') + 1);
 
 console.log('Scanning backward on channel:', channelName);
-console.log('Starting at: LATEST');
+console.log('Starting at:', timestamp + 'T23:59:59.999Z');
 
-hubClient.getItem(channelName, 'latest').then(function (item) {
-    process.stdout.write('.');
-    searchForKeyword(item);
-    goToPrevious(item);
-}).catch(function (error) {
-    console.log('\nERROR:', error);
+hubClient.getURIs(channelName, timestamp).then(function (uris) {
+    var uri = parseItemID(uris[uris.length - 1]);
+    hubClient.getItem(channelName, uri).then(function (item) {
+        process.stdout.write('.');
+        searchForKeyword(item);
+        goToPrevious(item);
+    }).catch(function (error) {
+        console.log('\nERROR:', error);
+    });
 });
+
+function parseItemID(uri) {
+    var uri = new URI(uri);
+    var path = uri.path();
+    return path.substr(32);
+}
 
 function searchForKeyword(item) {
     var content = JSON.stringify(item.content);
