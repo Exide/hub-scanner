5878051be1cdc386429d13ff5c40abe598ffc0f2
Ryan Zander <exide@hotmail.com>
2016-06-15 12:20:17 -0700

    Minor refactor.

--- scanBackwardsFromLatest.js
+++ scanBackwardsFromLatest.js
@@ -7,14 +7,12 @@ var keyword = process.argv[3];
 
 var hubURI = URI(channelURI).authority();
 var hubClient = new HubClient(hubURI);
-
-// get latest item from channel
 var uriPath = URI(channelURI).path();
 var channelName = uriPath.substr(uriPath.lastIndexOf('/') + 1);
+
 console.log('Scanning channel:', channelName);
 hubClient.getItem(channelName, 'latest').then(function (item) {
     process.stdout.write('.');
-    // console.log('Searching:', item.uri);
     searchForKeyword(item);
     goToPrevious(item);
 }).catch(function (error) {
@@ -32,7 +30,6 @@ function goToPrevious(item) {
     var previousItemID = new URI(item.links.previous).path().substr(32);
     hubClient.getItem(channelName, previousItemID).then(function (previousItem) {
         process.stdout.write('.');
-        // console.log('Searching:', item.uri);
         searchForKeyword(previousItem);
         goToPrevious(previousItem)
     }).catch(function (error) {
